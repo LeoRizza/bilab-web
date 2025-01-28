313eb282fd257245bec67271997ede88073d9c40
lrizza <leonardorizza@hotmail.com>
2025-01-27 23:29:02 -0300

    92.36 porciento web

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-/* const faqItems = document.querySelectorAll('.faq-item');
-
-    faqItems.forEach(item => {
-        const question = item.querySelector('.faq-question');
-        const icon = item.querySelector('.faq-icon');
-
-        question.addEventListener('click', () => {
-            const isOpen = item.classList.contains('open');
-
-            // Cierra todos los demás desplegables
-            faqItems.forEach(i => {
-                i.classList.remove('open');
-                i.querySelector('.faq-icon').textContent = '+'; // Cambia íconos a +
-            });
-
-            // Alterna el estado del actual
-            if (!isOpen) {
-                item.classList.add('open');
-                icon.textContent = '-'; // Muestra el ícono de cerrar
-            } else {
-                item.classList.remove('open');
-                icon.textContent = '+'; // Muestra el ícono de abrir
-            }
-        });
-    }); */
-
-    const faqItems = document.querySelectorAll('.faq-item');
+const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
     const question = item.querySelector('.faq-question');
@@ -55,4 +29,12 @@ faqItems.forEach(item => {
             icon.textContent = '-';
         }
     });
+});
+
+const textarea = document.getElementById('mensaje');
+
+// Evento para ajustar la altura automáticamente
+textarea.addEventListener('input', function () {
+    this.style.height = 'auto'; // Restablece la altura para recalcular
+    this.style.height = this.scrollHeight + 'px'; // Ajusta la altura al contenido
 });
